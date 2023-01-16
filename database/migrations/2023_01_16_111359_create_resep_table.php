bff0fe940831c2bca98ae5d68a6f42db2da36310
kevin azela <108333831+Rexoll@users.noreply.github.com>
2023-01-16 19:23:08 +0700

    Initial commit

---
+++ database/migrations/2023_01_16_111359_create_resep_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resep', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_resep');
+            $table->string('vidio');
+            $table->text('deskripsi');
+            $table->string('alat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resep');
+    }
+};
