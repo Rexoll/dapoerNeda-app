ee29f068b80d56ece6c1d465e7e209c591532534
kevin azela <108333831+Rexoll@users.noreply.github.com>
2023-01-30 19:57:38 +0700

    update 30/1/2023

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -10,6 +10,44 @@
 </head>
 
 <body>
+    <div class="">
+        <nav style="background-color: #FCAE1E;" class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand fs-4 ms-4 text-white" href="{{ route('user.dashboard') }}">DapoerNeda</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse me-5" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home') }}">
+                                <stronng class="text-white">Register</stronng>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('user.daftar.products') }}">
+                                <storng class="text-white">Resep</strong>
+                            </a>
+                        </li>
+                        <li>
+                        </li>
+                    </ul>
+                    <div class="d-flex justify-content-end">
+                        <div class="dropdown-center dropdown-toggle text-white">
+                            <button class="btn text-white" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <h5>Halo, {{ auth()->user()->name }}</h5>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="logout nav-link m-1" href="{{ route('logout') }}">logout</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+        </nav>
+    </div>
     <div class="container d-flex justify-content-center mt-5">
         <div class="row">
             <div class="col-md-12">
