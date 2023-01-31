1b0c4141d8a8a291cd3dc76bed485e353dea7580
kevin azela <108333831+Rexoll@users.noreply.github.com>
2023-01-31 18:06:12 +0700

    Update resep-edit.blade.php

--- resources/views/admin/resep-edit.blade.php
+++ resources/views/admin/resep-edit.blade.php
@@ -25,29 +25,33 @@
                 @csrf
                 @method('PUT')
                 <div class="card-body">
-                    <div class="form-group">
+                    <div class="form-group mb-3">
                         <label for="nama_resep">Nama Resep</label>
                         <input type="text" class="form-control" id="nama_resep" name="nama_resep" placeholder="Resep Rendang" value="{{ $resep->nama_resep }}">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group mb-3">
                         <label for="vidio">Link Vidio</label>
                         <input type="text" class="form-control" id="vidio" name="vidio" placeholder="vidio url" value="{{ $resep->vidio }}">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group mb-3">
                         <label for="vidio">thumbnail</label>
                         <input type="file" class="form-control" id="thumbnail" name="thumbnail" placeholder="Mohon Sertakan Foto Makanannya" value="{{ $resep->thumbnail }}">
                     </div>
-                    <div class="form-group">
+                    <div class="form-group mb-3">
                         <label for="deskripsi">Deskripsi</label>
                         <input type="text" class="form-control" id="deskripsi" name="deskripsi" placeholder="Rendang sangat lah lezat" value="{{ $resep->deskripsi }}">
                     </div>
                     <div class="form-group">
-                        <label for="alat">Masukkan Bahan Yang Dibutuhkan</label>
-                        <input type="text" class="form-control" id="alat" name="alat" placeholder="bawang,cabe,kol" value="{{ $resep->alat }}">
+                        <div class="mb-3">
+                            <label for="kategori" class="form-label">Masukkan bahan bahan yang dibutuhkan</label>
+                            <textarea class="form-control" id="kategori" name="kategori" placeholder="Bawang,tomat,garam" rows="3" value="{{ $resep->kategori }}"></textarea>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="kategori">Cara Pembuatan</label>
-                        <input type="text" class="form-control" id="kategori" name="kategori" placeholder="Masukkan cara Pembuatan" value="{{ $resep->kategori }}">
+                    <div class="form-group mb-3">
+                        <div class="mb-3">
+                            <label for="alat" class="form-label">Cara Pembuatan</label>
+                            <textarea class="form-control" id="alat" rows="3" name="alat" placeholder="Masukkan Cara Pembuatan Disini" value="{{ $resep->alat }}"></textarea>
+                        </div>
                     </div>
                 </div>
                 <!-- /.card-body -->
